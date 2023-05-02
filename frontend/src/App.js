80f7cc3457f0910c7b4479d06fffd710d44cf732
pramudithaSJ <IT20620202@my.sliit.lk>
2023-05-02 22:49:26 +0530

    Frontend Setup

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div className='bg-black'>
+      <h2 className='text-yellow-300'>
+        Hello World ` `
+      </h2>
+     </div>
+    </div>
+  );
+}
+
+export default App;
