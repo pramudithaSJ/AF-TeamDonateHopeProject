94b670581ff044d62186f2800cdd478013eedf0b
PramudithaJayawardhana <89015836+pramudithaSJ@users.noreply.github.com>
2023-05-03 23:57:44 +0530

    It20620202 (#5)
    * Create home.js
    
    * User View of IT20620202
    
    * HomePage Design
    
    * admin panel name updated (#4) (#6)
    
    Co-authored-by: Kushan Migara Ranasinghe <88665438+Kushan921@users.noreply.github.com>
    Co-authored-by: Kushan <kushanmigara@gmail.com>
    
    ---------
    
    Co-authored-by: Kushan Migara Ranasinghe <88665438+Kushan921@users.noreply.github.com>
    Co-authored-by: Kushan <kushanmigara@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,20 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Dashboard from "./IT20600884/dashboard";
 import AllDoctors from "./IT20600884/Doctor/AllDoctors";
 import AllMasters from "./IT20600884/Master/AllMasters";
-
-
+import HomePage from "./IT20620202/UserView/home";
 
 function App() {
   return (
     <div>
-     
       <BrowserRouter>
         <Routes>
+          <Route path="/" exact element={<HomePage />} />
           <Route path="/dashboard" exact element={<Dashboard />} />
-          <Route path="/doctors" exact element={<AllDoctors/>} />
-          <Route path="/masters" exact element={<AllMasters/>} />
+          <Route path="/doctors" exact element={<AllDoctors />} />
+          <Route path="/masters" exact element={<AllMasters />} />
         </Routes>
       </BrowserRouter>
     </div>
