540ed562a0c1cd6a14486c68f3a0dfa15ca37cc8
Kushan <kushanmigara@gmail.com>
2023-05-03 14:08:36 +0530

    doctor and master backend

---
+++ backend/server.js
@@ -0,0 +1,48 @@
+const express = require("express"); //using the json file dependencies(node_modules)
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+//declare a constant variable
+const app = express();
+//require  for read variables(MONGODB_URL)
+require("dotenv").config();
+
+app.use(cors());
+var bodyParser = require('body-parser');              
+app.use(bodyParser.json());
+
+
+//database link
+const URL = process.env.MONGODB_URL;
+
+const PORT = process.env.PORT || 8020;
+//create mongo configurations
+
+mongoose.connect(URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+
+const connection = mongoose.connection;
+connection.once("open",()=>{
+    console.log("mongoDB connection successful !!!");
+})
+
+// doctor
+const doctorRouter = require('./routes/DoctorRoute')
+app.use("/doctor",doctorRouter);
+
+// master
+const masterRouter = require('./routes/MasterRoute')
+app.use("/master",masterRouter);
+
+
+//run the app using port
+app.listen(PORT, () =>{
+    console.log(`Server is up and running on port number: ${PORT}`);
+
+})
+
+   
